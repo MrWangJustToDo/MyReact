27c5240f4fedf75c01c99fc98cf8f24df72b8c6c
MrWangJustToDo <2711470541@qq.com>
2023-05-31 21:09:36 +0800

    add a dev flow log

--- packages/myreact-reconciler/src/processState/feature.ts
+++ packages/myreact-reconciler/src/processState/feature.ts
@@ -1,12 +1,23 @@
+import { __my_react_internal__ } from "@my-react/react";
 import { ListTree, STATE_TYPE } from "@my-react/react-shared";
 
 import type { UpdateQueue } from "@my-react/react";
+
+const { currentComponentFiber, currentRenderPlatform } = __my_react_internal__;
 
 export const processState = (_params: UpdateQueue) => {
   if (_params.type === "component") {
     const ownerFiber = _params.trigger._ownerFiber;
 
     if (!ownerFiber || ownerFiber.state & STATE_TYPE.__unmount__) return;
+
+    if (__DEV__ && currentComponentFiber.current) {
+      currentRenderPlatform.current?.log({
+        fiber: currentComponentFiber.current,
+        message: `trigger an update when current update flow is running, this is a unexpected behavior, please make sure current render function is a pure function`,
+        level: "warn",
+      });
+    }
 
     ownerFiber.updateQueue = ownerFiber.updateQueue || new ListTree();
 
@@ -17,6 +28,14 @@ export const processState = (_params: UpdateQueue) => {
     const ownerFiber = _params.trigger._ownerFiber;
 
     if (!ownerFiber || ownerFiber?.state & STATE_TYPE.__unmount__) return;
+
+    if (__DEV__ && currentComponentFiber.current) {
+      currentRenderPlatform.current?.log({
+        fiber: currentComponentFiber.current,
+        message: `trigger an update when current update flow is running, this is a unexpected behavior, please make sure current render function is a pure function`,
+        level: "warn",
+      });
+    }
 
     ownerFiber.updateQueue = ownerFiber.updateQueue || new ListTree();
 

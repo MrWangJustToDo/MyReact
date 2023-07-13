8c851b27eb9d28ae585f8c9d636e3a78781189d7
MrWangJustToDo <2711470541@qq.com>
2023-07-13 22:33:49 +0800

    fix build script

--- scripts/rollupBuild.ts
+++ scripts/rollupBuild.ts
@@ -3,15 +3,15 @@ import { rollupBuild } from "project-tool/rollup";
 const external = (id: string) => id.includes("@my-react/") || id.includes("node_modules");
 
 const start = async () => {
-  // await rollupBuild({ packageName: "myreact-shared", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact-jsx", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact-reconciler", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact-dom", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact-reactivity", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "myreact-refresh", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-shared", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-jsx", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-reconciler", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-dom", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-reactivity", packageScope: "packages", external });
+  await rollupBuild({ packageName: "myreact-refresh", packageScope: "packages", external });
   await rollupBuild({ packageName: "myreact-refresh-next", packageScope: "packages", external });
-  // await rollupBuild({ packageName: "graphql", packageScope: "site", external });
+  await rollupBuild({ packageName: "graphql", packageScope: "site", external });
   process.exit(0);
 };
 

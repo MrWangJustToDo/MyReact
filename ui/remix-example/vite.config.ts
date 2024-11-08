a278dcd282a2684fcb039a6c19fc2e027daf2dca
MrWangJustToDo <2711470541@qq.com>
2024-11-08 23:38:31 +0800

    comment

--- ui/remix-example/vite.config.ts
+++ ui/remix-example/vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 import react from "@my-react/react-vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// !build will fail when using monorepo symlinks, npm install works fine, so build always fails in this case
 export default defineConfig({
   plugins: [
     react({ remix: true }),
     remix({
       future: {
+        // v3_singleFetch: true,
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
--- ui/vite-ssr-example/vite.config.ts
+++ ui/vite-ssr-example/vite.config.ts
@@ -2,6 +2,8 @@ import react from "@my-react/react-vite";
 // import react from '@vitejs/plugin-react';
 import { defineConfig } from "vite";
 
+// !SEE https://github.com/vitejs/vite/issues/12738
+// !build will fail when using monorepo symlinks, npm install works fine, so build always fails in this case
 // https://vitejs.dev/config/
 export default defineConfig({
   ssr: {
@@ -33,22 +35,5 @@ export default defineConfig({
       "framer-motion",
     ],
   },
-  // !SEE https://github.com/vitejs/vite/issues/12738
-  // !build will fail when using monorepo symlinks, npm install works fine, so build always fails in this case
-  // build: {
-  //   commonjsOptions: {
-  //     include: [
-  //       "@my-react/react",
-  //       "@my-react/react-dom",
-  //       "@my-react/react-dom/server",
-  //       "@my-react/react-dom/client",
-  //       "@my-react/react/jsx-runtime",
-  //       "@my-react/react/jsx-dev-runtime",
-  //       "react-router",
-  //       "react-router-dom",
-  //       "react-router-dom/server",
-  //     ],
-  //   },
-  // },
   plugins: [react()],
 });

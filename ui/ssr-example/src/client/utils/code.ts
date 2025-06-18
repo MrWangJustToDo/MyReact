2b448ad37f99d92aafe0aef744c29b6c57519c46
MrWangJustToDo <2711470541@qq.com>
2025-06-18 13:22:27 +0800

    update

--- ui/ssr-example/src/client/utils/code.ts
+++ ui/ssr-example/src/client/utils/code.ts
@@ -380,4 +380,53 @@ export const Exp = function Group() {
   );
 };
 `,
+  "Reparenting.tsx": `import { Canvas, createPortal } from "@my-react/react-three-fiber";
+import { useCallback, useEffect, useReducer, useState } from "react";
+
+
+import type * as THREE from "three";
+
+function Icosahedron() {
+  const [active, setActive] = useState(false);
+  const handleClick = useCallback(() => setActive((state) => !state), []);
+  return (
+    <mesh scale={active ? [2, 2, 2] : [1, 1, 1]} onClick={handleClick}>
+      <icosahedronGeometry args={[1, 0]} />
+      <meshNormalMaterial />
+    </mesh>
+  );
+}
+
+function RenderToPortal({ targets }: { targets: THREE.Group[] }) {
+  const [target, toggle] = useReducer((state) => (state + 1) % targets.length, 0);
+
+  useEffect(() => {
+    const interval = setInterval(toggle, 1000);
+    return () => clearInterval(interval);
+  }, [targets]);
+
+  return <>{createPortal(<Icosahedron />, targets[target])}</>;
+}
+
+export const Exp = function Group() {
+  const [ref1, set1] = useState<THREE.Group>(null!);
+  const [ref2, set2] = useState<THREE.Group>(null!);
+
+  console.log("rendering Group", ref1, ref2);
+
+  return (
+    <Canvas onCreated={() => console.log("onCreated")}>
+      <group>
+        <group ref={set1 as any} position={[-2, 0, 0]} />
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry args={[0.5, 16, 16]} />
+          <meshNormalMaterial />
+        </mesh>
+        <group ref={set2 as any} position={[2, 0, 0]} />
+        {ref1 && ref2 && <RenderToPortal targets={[ref1, ref2]} />}
+      </group>
+    </Canvas>
+  );
+};
+`
 };
